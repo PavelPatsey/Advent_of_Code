e81d140e36a7401973fc0008d110a8de657455f7
Павел Пацей <patsey.pavel@gmail.com>
2022-12-03 23:12:16 +0300

    cosmetic changes

--- 01_Calorie_Counting/01.py
+++ 01_Calorie_Counting/01.py
@@ -12,12 +12,9 @@ def main():
     calories = get_calories()
     sum_calories = list(map(sum, calories))
     max_calories = max(sum_calories)
-
-    # part_1 answer
     print(max_calories)
-
+    
     sorted_sum_calories = sorted(sum_calories, reverse=True)
-    # part 2 answer
     print(sum(sorted_sum_calories[:3]))
 
 
