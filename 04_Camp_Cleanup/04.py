3eda78d658c94ef58a9d1be7e98e5fb58f230880
Павел Пацей <patsey.pavel@gmail.com>
2022-12-04 11:46:55 +0300

    solve day 4 part 2

--- 04_Camp_Cleanup/04.py
+++ 04_Camp_Cleanup/04.py
@@ -1,6 +1,13 @@
 import re
+from functools import reduce
 
 INPUT = "input"
+
+
+def is_intersected(assignment):
+    a = set(range(assignment[0], assignment[1] + 1))
+    b = set(range(assignment[2], assignment[3] + 1))
+    return len(a.intersection(b)) != 0
 
 
 def is_overlapped(assignment):
@@ -20,11 +27,16 @@ def read_input():
 def main():
     assignments = read_input()
     print(len(list(filter(is_overlapped, assignments))))
+    print(len(list(filter(is_intersected, assignments))))
 
 
 if __name__ == "__main__":
     assert is_overlapped([2, 4, 6, 8]) == False
     assert is_overlapped([6, 6, 4, 6]) == True
+
+    assert is_intersected([2, 4, 6, 8]) == False
+    assert is_intersected([2, 4, 4, 4]) == True
+    assert is_intersected([2, 8, 6, 9]) == True
 
     main()
 
