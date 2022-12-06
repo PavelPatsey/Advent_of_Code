f9bc3e82de70e3143705a0bd143d8459a409ffcb
Павел Пацей <patsey.pavel@gmail.com>
2022-12-06 10:39:56 +0300

    solve day 06 part 2

--- 06_Tuning_Trouble/06.py
+++ 06_Tuning_Trouble/06.py
@@ -1,24 +1,34 @@
-def get_index(string):
+def get_index(string, number):
     string = list(string)
     N = len(string)
-    i = 3
+    i = number - 1
     while i < N:
-        if len(set([string[i], string[i - 1], string[i - 2], string[i - 3]])) == 4:
+        characters = []
+        for j in range(number):
+            characters.append(string[i - j])
+
+        if len(set(characters)) == number:
             return i + 1
         i += 1
+
 
 def read_input():
     with open("input", "r") as file:
         string = file.read()
     return string
 
+
 def main():
     string = read_input()
-    print(get_index(string))
+    print(get_index(string, 4))
+    print(get_index(string, 14))
 
 
 if __name__ == "__main__":
-    assert get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
-    assert get_index("nppdvjthqldpwncqszvftbrmjlhg") == 6
+    assert get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7
+    assert get_index("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
+
+    assert get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19
+    assert get_index("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23
 
     main()
