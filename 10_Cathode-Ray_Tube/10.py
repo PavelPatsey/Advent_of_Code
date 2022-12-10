93a44b10d53026811ad7f50ee033bfbaf7da5656
Павел Пацей <patsey.pavel@gmail.com>
2022-12-10 19:28:35 +0300

    solve day 10 part 2

--- 10_Cathode-Ray_Tube/10.py
+++ 10_Cathode-Ray_Tube/10.py
@@ -27,10 +27,24 @@ def read_input():
 def main():
     commands = read_input()
     values = get_values(commands)
+    print(list(enumerate(values)))
 
     filtered = filter(lambda x: (x[0] - 20) % 40 == 0, enumerate(values))
     mapped = map(lambda x: x[0] * x[1], filtered)
     print(sum(mapped))
+
+    str_stack = ""
+    for i in range(1, 240 + 1):
+        I = (i - 1) % 40
+        if I in (values[i] - 1, values[i], values[i] + 1):
+            str_stack += "#"
+        else:
+            str_stack += "."
+        if I == 39:
+            print(str_stack)
+            str_stack = ""
+
+    # print(str_stack)
 
 
 if __name__ == "__main__":
