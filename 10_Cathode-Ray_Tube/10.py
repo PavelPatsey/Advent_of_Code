b5f941ddd389c6a70a7c39093f1e128b9911e7da
Павел Пацей <patsey.pavel@gmail.com>
2022-12-10 22:58:16 +0300

    refactoring

--- 10_Cathode-Ray_Tube/10.py
+++ 10_Cathode-Ray_Tube/10.py
@@ -1,6 +1,22 @@
 INPUT = "input"
 
 
+def get_CRT_rows(values):
+    CRT_row = ""
+    CRT_rows = []
+    for i in range(1, 240 + 1):
+        pixel_position = (i - 1) % 40
+        if pixel_position in (values[i] - 1, values[i], values[i] + 1):
+            CRT_row += "#"
+        else:
+            CRT_row += "."
+        if pixel_position == 39:
+            CRT_rows.append(CRT_row)
+            CRT_row = ""
+
+    return CRT_rows
+
+
 def get_values(commands):
     x = 1
     values = [x]
@@ -27,24 +43,14 @@ def read_input():
 def main():
     commands = read_input()
     values = get_values(commands)
-    print(list(enumerate(values)))
 
     filtered = filter(lambda x: (x[0] - 20) % 40 == 0, enumerate(values))
     mapped = map(lambda x: x[0] * x[1], filtered)
     print(sum(mapped))
 
-    str_stack = ""
-    for i in range(1, 240 + 1):
-        I = (i - 1) % 40
-        if I in (values[i] - 1, values[i], values[i] + 1):
-            str_stack += "#"
-        else:
-            str_stack += "."
-        if I == 39:
-            print(str_stack)
-            str_stack = ""
-
-    # print(str_stack)
+    CRT_rows = get_CRT_rows(values)
+    for row in CRT_rows:
+        print(row)
 
 
 if __name__ == "__main__":
