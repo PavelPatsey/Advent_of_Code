2f23f20076864c9399e97f2ac1042958ee3e3a51
Павел Пацей <patsey.pavel@gmail.com>
2022-12-15 14:43:34 +0300

    add function print_matrix

--- 15_Beacon_Exclusion_Zone/15.py
+++ 15_Beacon_Exclusion_Zone/15.py
@@ -8,6 +8,7 @@ def read_input():
         data = file.read().strip().splitlines()
     sensor_set = set()
     beacon_set = set()
+    sensor_set_x_y = set()
     for line in data:
         numbers = re.split(r"\D", line)
         numbers = list(map(int, filter(lambda x: x != "", numbers)))
@@ -17,9 +18,29 @@ def read_input():
         b_y = numbers[3]
         d = abs(s_x - b_x) + abs(s_y - b_y)
         sensor_set.add((s_x, s_y, d))
+        sensor_set_x_y.add((s_x, s_y))
         beacon_set.add((b_x, b_y))
 
-    return sensor_set, beacon_set
+    return sensor_set, sensor_set_x_y, beacon_set
+
+
+def print_matrix(sensor_set, sensor_set_x_y, beacon_set):
+    N = 20
+    matrix = [["." for y in range(N)] for x in range(N)]
+
+    for y in range(N):
+        for x in range(N):
+            if (x, y) in beacon_set:
+                matrix[y][x] = "B"
+            elif (x, y) in sensor_set_x_y:
+                matrix[y][x] = "S"
+            elif is_cannot_possibly_exist(x, y, sensor_set, beacon_set):
+                matrix[y][x] = "#"
+            else:
+                pass
+
+    for y in range(N):
+        print("".join(matrix[y]))
 
 
 def is_cannot_possibly_exist(x, y, sensor_set, beacon_set):
@@ -31,6 +52,19 @@ def is_cannot_possibly_exist(x, y, sensor_set, beacon_set):
             if d_x_y <= d:
                 return True
         return False
+
+
+def is_beacon(x, y, sensor_set, beacon_set):
+    result = True
+    if (x, y) in beacon_set:
+        result = False
+    else:
+        for (s_x, s_y, d) in sensor_set:
+            d_x_y = abs(s_x - x) + abs(s_y - y)
+            if d_x_y <= d:
+                result = result and False
+
+    return result
 
 
 def get_answer_1(sensor_set, beacon_set):
@@ -52,10 +86,34 @@ def get_answer_1(sensor_set, beacon_set):
     return counter
 
 
+def get_answer_2(sensor_set, beacon_set):
+    if INPUT == "test_input":
+        min_y = 0
+        max_y = 20
+        min_x = 0
+        max_x = 20
+    else:
+        min_y = 0
+        max_y = 4_000_000
+        min_x = 0
+        max_x = 4_000_000
+
+    for x in range(min_x, max_x):
+        for y in range(min_y, max_y):
+            if is_beacon(x, y, sensor_set, beacon_set):
+                result = x * 4_000_000 + y
+                print(x, y)
+                break
+
+    return result
+
+
 def main():
-    sensor_set, beacon_set = read_input()
+    sensor_set, sensor_set_x_y, beacon_set = read_input()
 
     print(get_answer_1(sensor_set, beacon_set))
+    print_matrix(sensor_set, sensor_set_x_y, beacon_set)
+    print(get_answer_2(sensor_set, beacon_set))
 
 
 if __name__ == "__main__":
