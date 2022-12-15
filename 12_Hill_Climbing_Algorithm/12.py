c0562df5a37296c47406fe8877d5de09a6486c2a
Павел Пацей <patsey.pavel@gmail.com>
2022-12-15 21:50:16 +0300

    solving part 1

--- 12_Hill_Climbing_Algorithm/12.py
+++ 12_Hill_Climbing_Algorithm/12.py
@@ -1,8 +1,6 @@
 import copy
 
-# B = copy.deepcopy(A)
-
-INPUT = "test_input"
+INPUT = "input"
 SYMBOLS = {
     (-1, 0): "^",
     (0, 1): ">",
@@ -14,7 +12,7 @@ SYMBOLS = {
 def read_input():
     with open(INPUT, "r") as file:
         data = file.read().strip().splitlines()
-    matrix = [[y for y in x] for x in data]
+    matrix = [[y if y != "E" else "{" for y in x] for x in data]
     return matrix
 
 
@@ -24,38 +22,37 @@ def print_matrix(matrix):
 
 
 def get_traversed_matrix(matrix, x, y, acc):
-    # if matrix[y][x] in ("^", "!", "<", ">"):
-    #     return
+    if matrix[y][x] in ("^", "!", "<", ">"):
+        return
 
-    if matrix[y][x] == "E":
+    if matrix[y][x] == "{":
         print(acc)
+        print_matrix(matrix)
         return matrix
 
     for (dy, dx) in ((-1, 0), (0, 1), (1, 0), (0, -1)):
         new_x = x + dx
         new_y = y + dy
         new_matrix = copy.deepcopy(matrix)
-        
-        if 0 <= new_y <= len(matrix) -1 and 0 <= new_x <= len(matrix[0]) -1 :
-            # print(new_y, new_x)  
+
+        if 0 <= new_y <= len(new_matrix) - 1 and 0 <= new_x <= len(new_matrix[0]) - 1:
+            # print(new_y, new_x)
             # print(len(matrix))
             # print(len(matrix[0]))
-            if new_matrix[new_y][new_x] == "E":
+            if abs(
+                ord(new_matrix[new_y][new_x]) - ord(new_matrix[y][x])
+            ) <= 1 and not new_matrix[new_y][new_x] in ("^", "!", "<", ">"):
+                print(new_y, new_x)
                 print(acc)
-                print(new_y, new_x)  
                 print_matrix(new_matrix)
-                return new_matrix
-            if abs(ord(new_matrix[new_y][new_x]) - ord(new_matrix[y][x])) <= 1:
-                # print(new_y, new_x)  
-                # print_matrix(new_matrix)
                 new_matrix[y][x] = SYMBOLS[dy, dx]
-                return get_traversed_matrix(new_matrix, new_x, new_y, acc + 1)
-
-
+                get_traversed_matrix(new_matrix, new_x, new_y, acc + 1)
+                
 
 
 def main():
     matrix = read_input()
+    # print_matrix(matrix)
 
     matrix[0][0] = "a"
     traversed_matrix = get_traversed_matrix(matrix, 0, 0, 0)
