61723606886b0ff49a10bc527a5a8f8c1124d5eb
Павел Пацей <patsey.pavel@gmail.com>
2022-12-16 01:14:19 +0300

    add рамку из $ вокруг таблицы

--- 12_Hill_Climbing_Algorithm/12.py
+++ 12_Hill_Climbing_Algorithm/12.py
@@ -12,7 +12,9 @@ SYMBOLS = {
 def read_input():
     with open(INPUT, "r") as file:
         data = file.read().strip().splitlines()
-    matrix = [[y if y != "E" else "{" for y in x] for x in data]
+    matrix = [[y for y in x] for x in data]
+    matrix = [["$"] * len(matrix[0])] + matrix + [["$"] * len(matrix[0])]
+    matrix = [["$"] + line + ["$"] for line in matrix]
     return matrix
 
 
@@ -21,9 +23,12 @@ def print_matrix(matrix):
         print("".join(matrix[y]))
 
 
-def get_traversed_matrix(matrix, x, y, acc):
-    if matrix[y][x] in ("^", "!", "<", ">"):
-        return
+def get_traversed_matrix(matrix, x, y, acc, visited_set):
+    char = matrix[y][x]
+    matrix[y][x] = "#"
+    visited_set.add((y,x))
+    # print(x,y)
+    # print_matrix(matrix)
 
     if matrix[y][x] == "{":
         print(acc)
@@ -34,28 +39,33 @@ def get_traversed_matrix(matrix, x, y, acc):
         new_x = x + dx
         new_y = y + dy
         new_matrix = copy.deepcopy(matrix)
+        new_visited_set = copy.deepcopy(visited_set)
 
-        if 0 <= new_y <= len(new_matrix) - 1 and 0 <= new_x <= len(new_matrix[0]) - 1:
-            # print(new_y, new_x)
-            # print(len(matrix))
-            # print(len(matrix[0]))
-            if abs(
-                ord(new_matrix[new_y][new_x]) - ord(new_matrix[y][x])
-            ) <= 1 and not new_matrix[new_y][new_x] in ("^", "!", "<", ">"):
-                print(new_y, new_x)
-                print(acc)
+        if not new_matrix[new_y][new_x] in ("$", "#"):
+            # print(new_y,new_x)
+            if ord(new_matrix[new_y][new_x]) - ord(char) <= 1:
+                print(new_matrix[new_y][new_x])
                 print_matrix(new_matrix)
-                new_matrix[y][x] = SYMBOLS[dy, dx]
-                get_traversed_matrix(new_matrix, new_x, new_y, acc + 1)
-                
+                get_traversed_matrix(new_matrix, new_x, new_y, acc + 1, new_visited_set)
 
 
 def main():
     matrix = read_input()
-    # print_matrix(matrix)
 
-    matrix[0][0] = "a"
-    traversed_matrix = get_traversed_matrix(matrix, 0, 0, 0)
+    E = None
+    S = None
+    for y in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            if matrix[y][x] == "E":
+                matrix[y][x] = "{"
+                E = (y, x)
+            if matrix[y][x] == "S":
+                matrix[y][x] = "a"
+                S = (y, x)
+
+    # print(S)
+    # print_matrix(matrix)
+    traversed_matrix = get_traversed_matrix(matrix, S[1], S[0], 0, set())
     # print_matrix(traversed_matrix)
 
 
