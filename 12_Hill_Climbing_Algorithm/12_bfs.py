aa11ba07c7660f9988d53b069a668c1d18189fcc
Павел Пацей <patsey.pavel@gmail.com>
2022-12-16 13:07:23 +0300

    solve day 12 part 2

--- 12_Hill_Climbing_Algorithm/12_bfs.py
+++ 12_Hill_Climbing_Algorithm/12_bfs.py
@@ -17,10 +17,37 @@ def print_matrix(matrix):
         print("".join(matrix[r]))
 
 
-def get_bfs_steps(letters, heights, S):
+def get_heights(letters):
+    R = len(letters)
+    C = len(letters[0])
+
+    heights = [[0 for _ in range(C)] for _ in range(R)]
+    for r in range(R):
+        for c in range(C):
+            if letters[r][c] == "$":
+                pass
+            elif letters[r][c] == "S":
+                heights[r][c] = 1
+            elif letters[r][c] == "E":
+                heights[r][c] = 26
+            else:
+                heights[r][c] = ord(letters[r][c]) - ord("a") + 1
+
+    return heights
+
+
+def get_bfs_steps(letters, heights, part):
     visited = set()
     queue = deque()
-    queue.append((S, 0))
+
+    R = len(letters)
+    C = len(letters[0])
+    for r in range(R):
+        for c in range(C):
+            if (part == 1 and letters[r][c] == "S") or (
+                part == 2 and heights[r][c] == 1
+            ):
+                queue.append(((r, c), 0))
 
     while queue:
         (r, c), d = queue.popleft()
@@ -38,25 +65,10 @@ def get_bfs_steps(letters, heights, S):
 
 def main():
     letters = read_input()
-    R = len(letters)
-    C = len(letters[0])
+    heights = get_heights(letters)
 
-    heights = [[0 for _ in range(C)] for _ in range(R)]
-
-    S = None
-    for r in range(R):
-        for c in range(C):
-            if letters[r][c] == "$":
-                continue
-            elif letters[r][c] == "S":
-                heights[r][c] = 1
-                S = (r, c)
-            elif letters[r][c] == "E":
-                heights[r][c] = 26
-            else:
-                heights[r][c] = ord(letters[r][c]) - ord("a") + 1
-
-    print(get_bfs_steps(letters, heights, S))
+    print(get_bfs_steps(letters, heights, part=1))
+    print(get_bfs_steps(letters, heights, part=2))
 
 
 if __name__ == "__main__":
