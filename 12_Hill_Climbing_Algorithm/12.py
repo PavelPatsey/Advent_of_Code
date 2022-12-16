e2dc14cf775a2905fde7f802e944ab074bbf16b0
Павел Пацей <patsey.pavel@gmail.com>
2022-12-16 11:42:29 +0300

    y,x -> r,c

--- 12_Hill_Climbing_Algorithm/12.py
+++ 12_Hill_Climbing_Algorithm/12.py
@@ -12,41 +12,35 @@ SYMBOLS = {
 def read_input():
     with open(INPUT, "r") as file:
         data = file.read().strip().splitlines()
-    matrix = [[y for y in x] for x in data]
+    matrix = [[char for char in row] for row in data]
     matrix = [["$"] * len(matrix[0])] + matrix + [["$"] * len(matrix[0])]
     matrix = [["$"] + line + ["$"] for line in matrix]
     return matrix
 
 
 def print_matrix(matrix):
-    for y in range(len(matrix)):
-        print("".join(matrix[y]))
+    for r in range(len(matrix)):
+        print("".join(matrix[r]))
 
 
-def get_traversed_matrix(matrix, x, y, acc, visited_set):
-    char = matrix[y][x]
-    matrix[y][x] = "#"
-    visited_set.add((y,x))
-    # print(x,y)
-    # print_matrix(matrix)
+def traverse_matrix(matrix, r, c, acc):
+    print(r, c)
+    print_matrix(matrix)
 
-    if matrix[y][x] == "{":
-        print(acc)
+    if matrix[r][c] in ("$", "#"):
+        return
+
+    if matrix[r][c] == "{":
+        print(f"{acc=}")
         print_matrix(matrix)
-        return matrix
+        return
 
-    for (dy, dx) in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-        new_x = x + dx
-        new_y = y + dy
-        new_matrix = copy.deepcopy(matrix)
-        new_visited_set = copy.deepcopy(visited_set)
+    char = matrix[r][c]
+    matrix[r][c] = "#"
 
-        if not new_matrix[new_y][new_x] in ("$", "#"):
-            # print(new_y,new_x)
-            if ord(new_matrix[new_y][new_x]) - ord(char) <= 1:
-                print(new_matrix[new_y][new_x])
-                print_matrix(new_matrix)
-                get_traversed_matrix(new_matrix, new_x, new_y, acc + 1, new_visited_set)
+    for (dr, dc) in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+        if ord(matrix[r + dr][c + dc]) <= 1 + ord(char):
+            traverse_matrix(copy.deepcopy(matrix), r + dr, c + dc, acc + 1)
 
 
 def main():
@@ -54,19 +48,18 @@ def main():
 
     E = None
     S = None
-    for y in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            if matrix[y][x] == "E":
-                matrix[y][x] = "{"
-                E = (y, x)
-            if matrix[y][x] == "S":
-                matrix[y][x] = "a"
-                S = (y, x)
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] == "E":
+                matrix[r][c] = "{"
+                E = (r, c)
+            if matrix[r][c] == "S":
+                matrix[r][c] = "a"
+                S = (r, c)
 
     # print(S)
     # print_matrix(matrix)
-    traversed_matrix = get_traversed_matrix(matrix, S[1], S[0], 0, set())
-    # print_matrix(traversed_matrix)
+    traverse_matrix(matrix, S[0], S[1], 0)
 
 
 if __name__ == "__main__":
