9fa5b3c0626c4eea3e1cfd9732b8e7b1fb82f3ad
Павел <patsey.pavel@gmail.com>
2023-08-24 16:58:00 +0300

    day 02 add get_round_outcome and get_total_round_score functions

--- aoc_2022_elixir/02_Rock_Paper_Scissors/02.exs
+++ aoc_2022_elixir/02_Rock_Paper_Scissors/02.exs
@@ -7,12 +7,35 @@ defmodule AssertionTest do
     assert true
   end
 
-  test "Shape score X" do
-    assert Day02.get_your_shape_score("X") == 0
+  test "get_shape_score" do
+    assert Day02.get_shape_score("X") == 0
+    assert Day02.get_shape_score("Y") == 1
+    assert Day02.get_shape_score("Z") == 2
+  end
+
+  test "get_round_outcome" do
+    assert Day02.get_round_outcome("A", "X") == 3
+    assert Day02.get_round_outcome("A", "Y") == 6
+    assert Day02.get_round_outcome("A", "Z") == 0
   end
 end
 
 defmodule Day02 do
+  @shape_index %{
+    "A" => 0,
+    "B" => 1,
+    "C" => 2,
+    "X" => 0,
+    "Y" => 1,
+    "Z" => 2
+  }
+
+  @round_outcome_matrix {
+    {3, 6, 0},
+    {0, 3, 6},
+    {6, 0, 3}
+  }
+
   def read_input do
     {:ok, file} = File.read("./test_input")
 
@@ -21,17 +44,15 @@ defmodule Day02 do
     |> Enum.map(&String.split/1)
   end
 
-  #   def get_total_round_score(elf_move, my_move) do
-  #     1
-  #   end
+  def get_total_round_score(elf_move, my_move) do
+    get_round_outcome(elf_move, my_move) + get_shape_score(my_move)
+  end
 
-  #   def get_round_outcome(elf_move, my_move) do
-  #     case {elf_move, my_move} do
-  #       {"A", "X"} -> 0
-  #     end
-  #   end
+  def get_round_outcome(elf_move, my_move) do
+    elem(elem(@round_outcome_matrix, @shape_index[elf_move]), @shape_index[my_move])
+  end
 
-  def get_your_shape_score(my_move) do
+  def get_shape_score(my_move) do
     case my_move do
       "X" -> 0
       "Y" -> 1
