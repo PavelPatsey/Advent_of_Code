02468e9a31a53e8ba4ba04207570ccd9ee73e9cd
Павел <patsey.pavel@gmail.com>
2023-08-24 16:27:13 +0300

    day 02 add get_your_shape_score function

--- aoc_2022_elixir/02_Rock_Paper_Scissors/02.exs
+++ aoc_2022_elixir/02_Rock_Paper_Scissors/02.exs
@@ -6,6 +6,10 @@ defmodule AssertionTest do
   test "the truth" do
     assert true
   end
+
+  test "Shape score X" do
+    assert Day02.get_your_shape_score("X") == 0
+  end
 end
 
 defmodule Day02 do
@@ -16,6 +20,27 @@ defmodule Day02 do
     |> String.split("\n")
     |> Enum.map(&String.split/1)
   end
+
+  #   def get_total_round_score(elf_move, my_move) do
+  #     1
+  #   end
+
+  #   def get_round_outcome(elf_move, my_move) do
+  #     case {elf_move, my_move} do
+  #       {"A", "X"} -> 0
+  #     end
+  #   end
+
+  def get_your_shape_score(my_move) do
+    case my_move do
+      "X" -> 0
+      "Y" -> 1
+      "Z" -> 2
+      _ -> "Incorrect shape"
+    end
+  end
 end
 
 rounds = Day02.read_input()
+
+Day02.get_round_outcome("A", "X") |> IO.inspect()
