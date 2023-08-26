de1f6bafd2b51451e52abe1147644662b5742582
Павел <patsey.pavel@gmail.com>
2023-08-26 19:18:08 +0300

    solve day04 part 2

--- aoc_2022_elixir/04_Camp_Cleanup/04.exs
+++ aoc_2022_elixir/04_Camp_Cleanup/04.exs
@@ -13,6 +13,12 @@ defmodule AssertionTest do
     assert Day04.is_contains([3, 7, 2, 8]) == true
     assert Day04.is_contains([2, 8, 3, 7]) == true
   end
+
+  test "test is_overlaps" do
+    assert Day04.is_overlaps([2, 4, 6, 8]) == false
+    assert Day04.is_overlaps([4, 6, 5, 8]) == true
+    assert Day04.is_overlaps([4, 6, 6, 8]) == true
+  end
 end
 
 defmodule Day04 do
@@ -28,10 +34,21 @@ defmodule Day04 do
   def is_contains([a1, a2, b1, b2]) do
     (a1 <= b1 and b2 <= a2) or (b1 <= a1 and a2 <= b2)
   end
+
+  def is_overlaps([a1, a2, b1, b2]) do
+    (b1 <= a2 and a2 <= b2) or
+      (b1 <= a1 and a1 <= b2) or
+      (a1 <= b1 and b1 <= a2) or
+      (a1 <= b2 and b2 <= a2)
+  end
 end
 
 assignments = Day04.read_input()
 
 Enum.filter(assignments, &Day04.is_contains/1)
+|> length()
+|> IO.inspect()
+
+Enum.filter(assignments, &Day04.is_overlaps/1)
 |> length()
 |> IO.inspect()
