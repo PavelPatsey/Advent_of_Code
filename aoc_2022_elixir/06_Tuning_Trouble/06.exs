9cc0360265329233a65bb1a58769654450bffef0
Павел <patsey.pavel@gmail.com>
2023-09-02 14:26:36 +0300

    solve day 06 part 2

--- aoc_2022_elixir/06_Tuning_Trouble/06.exs
+++ aoc_2022_elixir/06_Tuning_Trouble/06.exs
@@ -11,6 +11,9 @@ defmodule AssertionTest do
     assert Day06.get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7
     assert Day06.get_index("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
     assert Day06.get_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10
+
+    assert Day06.get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19
+    assert Day06.get_index("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23
   end
 end
 
@@ -40,7 +43,7 @@ defmodule Day06 do
          |> MapSet.new()
          |> MapSet.size(), index}
       end)
-      |> Enum.find(fn x -> elem(x, 0) == 4 end)
+      |> Enum.find(fn x -> elem(x, 0) == number end)
       |> elem(1)
 
     index + number
@@ -51,3 +54,6 @@ data = Day06.read_input("./input")
 
 Day06.get_index(data, 4)
 |> IO.inspect(label: "part 1")
+
+Day06.get_index(data, 14)
+|> IO.inspect(label: "part 2")
