f7b96025989703e50c0d16bfff3ba47677803d99
Павел <patsey.pavel@gmail.com>
2023-09-02 14:21:16 +0300

    solve day 06 part 1

---
+++ aoc_2022_elixir/06_Tuning_Trouble/06.exs
@@ -0,0 +1,53 @@
+ExUnit.start()
+
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
+
+  test "the truth" do
+    assert true
+  end
+
+  test "test get_index" do
+    assert Day06.get_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7
+    assert Day06.get_index("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
+    assert Day06.get_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10
+  end
+end
+
+defmodule Day06 do
+  def read_input(path) do
+    {:ok, file} = File.read(path)
+
+    file
+    |> String.trim()
+  end
+
+  def get_index(data, number) do
+    zipped =
+      for n <- 1..number do
+        Enum.drop(String.to_charlist(data), n - 1)
+      end
+      |> Enum.zip()
+      |> Enum.with_index()
+
+    index =
+      zipped
+      |> Enum.map(fn x ->
+        {tuple, index} = x
+
+        {tuple
+         |> Tuple.to_list()
+         |> MapSet.new()
+         |> MapSet.size(), index}
+      end)
+      |> Enum.find(fn x -> elem(x, 0) == 4 end)
+      |> elem(1)
+
+    index + number
+  end
+end
+
+data = Day06.read_input("./input")
+
+Day06.get_index(data, 4)
+|> IO.inspect(label: "part 1")
