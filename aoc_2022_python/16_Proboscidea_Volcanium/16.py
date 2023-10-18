f7d791e052f9ab8559a2fa6d780a3e0d18a274b4
Павел <patsey.pavel@gmail.com>
2023-10-18 17:30:47 +0300

    added read_input

--- aoc_2022_python/16_Proboscidea_Volcanium/16.py
+++ aoc_2022_python/16_Proboscidea_Volcanium/16.py
@@ -1,5 +1,37 @@
+import re
+
+INPUT = "test_input"
+
+
+class Valve:
+    def __init__(self, name, flow_rate, connections):
+        self.name = name
+        self.flow_rate = flow_rate
+        self.connections = connections
+
+
+class Graph:
+    pass
+
+
+def read_input():
+    def split(string):
+        return re.split("Valve | has flow rate=|; tunnels lead to valves ", string)[1:]
+
+    with open(INPUT, "r") as file:
+        data = file.read().strip().splitlines()
+    return list(map(split, data))
+
+
+def get_valves(data):
+    pass
+
+
 def main():
-    print("start")
+    data = read_input()
+    print(data)
+    # valves = get_valves(INPUT)
+    # print(valves)
 
 
 if __name__ == "__main__":
