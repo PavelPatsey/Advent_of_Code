0bde4581ba9a6f24da0f55290dde4f364ecce2d1
Павел <patsey.pavel@gmail.com>
2023-10-18 19:21:55 +0300

    added str method in class Valve, added function get_valves

--- aoc_2022_python/16_Proboscidea_Volcanium/16.py
+++ aoc_2022_python/16_Proboscidea_Volcanium/16.py
@@ -9,6 +9,9 @@ class Valve:
         self.flow_rate = flow_rate
         self.connections = connections
 
+    def __str__(self):
+        return f"name = {self.name}, flow_rate = {self.flow_rate}, connection = {self.connections}"
+
 
 class Graph:
     pass
@@ -27,14 +30,19 @@ def read_input():
 
 
 def get_valves(data):
-    pass
+    def _make_valve(valve_attributes):
+        name, flow_rate, connections = valve_attributes
+        return Valve(name, int(flow_rate), connections.split())
+
+    valves = list(map(_make_valve, data))
+    return valves
 
 
 def main():
     data = read_input()
-    print(data)
-    # valves = get_valves(INPUT)
-    # print(valves)
+    valves = get_valves(data)
+    for valve in valves:
+        print(valve)
 
 
 if __name__ == "__main__":
