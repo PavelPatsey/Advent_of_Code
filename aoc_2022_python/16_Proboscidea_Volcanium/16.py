0752c48a826bb3cdc7abb42f46550a43ce34a05a
Павел <patsey.pavel@gmail.com>
2023-10-18 19:33:18 +0300

    combined the functions read_input and get_valves

--- aoc_2022_python/16_Proboscidea_Volcanium/16.py
+++ aoc_2022_python/16_Proboscidea_Volcanium/16.py
@@ -17,30 +17,27 @@ class Graph:
     pass
 
 
-def read_input():
+def get_valves():
     def _split(string):
         return re.split(
             "Valve | has flow rate=|; tunnels lead to valves |; tunnel leads to valve ",
             string,
         )[1:]
 
-    with open(INPUT, "r") as file:
-        data = file.read().strip().splitlines()
-    return list(map(_split, data))
-
-
-def get_valves(data):
     def _make_valve(valve_attributes):
         name, flow_rate, connections = valve_attributes
         return Valve(name, int(flow_rate), connections.split())
 
-    valves = list(map(_make_valve, data))
+    with open(INPUT, "r") as file:
+        data = file.read().strip().splitlines()
+
+    valve_attributes = list(map(_split, data))
+    valves = list(map(_make_valve, valve_attributes))
     return valves
 
 
 def main():
-    data = read_input()
-    valves = get_valves(data)
+    valves = get_valves()
     for valve in valves:
         print(valve)
 
