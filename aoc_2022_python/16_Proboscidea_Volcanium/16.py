73822b287ea645119ae54477706a62450c544cfb
Павел <patsey.pavel@gmail.com>
2023-10-20 22:06:53 +0300

    writing get_all_travels

--- aoc_2022_python/16_Proboscidea_Volcanium/16.py
+++ aoc_2022_python/16_Proboscidea_Volcanium/16.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 INPUT = "test_input"
 ROOT_NAME = "AA"
-TIME_LIMIT = 20
+TIME_LIMIT = 10
 
 
 class Valve:
@@ -12,7 +12,6 @@ class Valve:
         self.name = name
         self.flow_rate = flow_rate
         self.connections = connections
-        self.is_opened = False
 
     def __str__(self):
         return f"name = {self.name}, flow_rate = {self.flow_rate}, connections = {self.connections}"
@@ -45,9 +44,13 @@ def get_all_travels(valves: Dict):
         valve_names = set(visited_valves_names)
         pressure_change = 0
         for valve_name in valve_names:
-            valve = valves[valve_name]
-            pressure_change += valve.flow_rate
+            valve = valves.get(valve_name)
+            if valve:
+                pressure_change += valve.flow_rate
         return pressure_change
+
+    def _all_valves_are_opened(visited_valves_names):
+        return False
 
     def _travers(
         go_to_valve_name,
@@ -57,7 +60,13 @@ def get_all_travels(valves: Dict):
     ):
         pressure_change = _get_pressure_change(visited_valves_names)
 
-        if past_minutes == TIME_LIMIT:
+        # если все клапаны открыты - выходить из функции
+        # _all_valves_is_opened должно считаться исходя visited_valves_names и valve_name + " opened"
+        # _get_pressure_change считать из аккумулятора аккумулятора давления
+
+        # если много повторных посещений - выходить из функции
+
+        if past_minutes == TIME_LIMIT or _all_valves_are_opened(visited_valves_names):
             travels.append(visited_valves_names)
             released_pressures.append(released_pressure + pressure_change)
             len_travels = len(travels)
@@ -68,6 +77,12 @@ def get_all_travels(valves: Dict):
         current_valve = valves[go_to_valve_name]
         if not (current_valve.flow_rate == 0 or current_valve.is_opened):
             current_valve.is_opened = True
+            _travers(
+                go_to_valve_name,
+                visited_valves_names + [go_to_valve_name + " opened"],
+                released_pressure + pressure_change,
+                past_minutes + 1,
+            )
 
         for valve_name in current_valve.connections:
             _travers(
@@ -76,6 +91,7 @@ def get_all_travels(valves: Dict):
                 released_pressure + pressure_change,
                 past_minutes + 1,
             )
+        return
 
     travels = []
     released_pressures = []
@@ -87,8 +103,9 @@ def main():
     t0 = time.time()
     valves = get_valves()
     travels, released_pressures = get_all_travels(valves)
-    # print(travels)
-    # print(released_pressures)
+    print(f"finished in {time.time() - t0:0f} sec")
+    print(travels[0])
+    print(released_pressures[0])
     print(f"finished in {time.time() - t0:0f} sec")
 
 
