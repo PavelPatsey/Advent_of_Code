a7ceb0db31d49b7849357514bcdf0aba14f33458
Павел <patsey.pavel@gmail.com>
2023-10-31 16:22:52 +0300

    corrected walk function with distances

--- aoc_2022_python/16_Proboscidea_Volcanium/16_01_with_distances.py
+++ aoc_2022_python/16_Proboscidea_Volcanium/16_01_with_distances.py
@@ -4,7 +4,7 @@ from collections import deque
 
 INPUT = "test_input"
 ROOT_NAME = "AA"
-TIME_LIMIT = 10
+TIME_LIMIT = 11
 
 
 def get_valves():
@@ -60,22 +60,20 @@ def get_bfs_shortest_path(graph, start, goal):
     return []
 
 
-def get_distances(valves, flow_rates, tunnels):
-    valve_list = list(filter(lambda x: flow_rates[x] != 0, valves))
-    if ROOT_NAME not in valve_list:
-        valve_list = [ROOT_NAME] + valve_list
+def get_distances(valves, tunnels):
     distances = {}
-    for i in valve_list:
-        for j in valve_list:
+    for i in valves:
+        for j in valves:
             length = len(get_bfs_shortest_path(tunnels, i, j))
             distances[i, j] = length - 1 if length != 0 else 0
     return distances
 
 
-def walk(flow_rates, tunnels):
-    def _walk(valve, dp, t, p_sum, opened):
+def walk(flow_rates, tunnels, distances):
+    def _walk(valve, dp, t, dt, p_sum, opened):
         a = 0
-        temp_p_sum = p_sum + dp
+        t = t + dt
+        temp_p_sum = p_sum + dp * dt
         if t == TIME_LIMIT:
             return p_sum
 
@@ -83,14 +81,16 @@ def walk(flow_rates, tunnels):
             temp_set = opened.copy()
             temp_set.add(valve)
             temp_dp = dp + flow_rates[valve]
-            a = _walk(valve, temp_dp, t + 1, temp_p_sum, temp_set)
+            a = _walk(valve, temp_dp, t, 1, temp_p_sum, temp_set)
 
         b = {}
-        for valve in tunnels[valve]:
-            b[valve] = _walk(valve, dp, t + 1, temp_p_sum, opened)
+        for got_to_valve in tunnels[valve]:
+            b[got_to_valve] = _walk(
+                got_to_valve, dp, t, distances[valve, got_to_valve], temp_p_sum, opened
+            )
         return max([a] + list(b.values()))
 
-    max_p_sum = _walk(ROOT_NAME, 0, 0, 0, set())
+    max_p_sum = _walk(ROOT_NAME, 0, 0, 0, 0, set())
     return max_p_sum
 
 
@@ -109,10 +109,14 @@ def main():
         "GG",
     ]
 
-    distances = get_distances(valves, flow_rates, tunnels)
+    non_zero_valves = list(filter(lambda x: flow_rates[x] != 0, valves))
+    if ROOT_NAME not in non_zero_valves:
+        non_zero_valves = [ROOT_NAME] + non_zero_valves
+
+    distances = get_distances(valves, tunnels)
     print(f"{distances=}")
 
-    max_p_sum = walk(flow_rates, tunnels)
+    max_p_sum = walk(flow_rates, tunnels, distances)
     print(max_p_sum)
 
     print(f"finished in {time.time() - t0:0f} sec")
