f9c90da830c55a704ba92ea01df53208d113a191
Павел <patsey.pavel@gmail.com>
2023-11-10 14:53:51 +0300

    corrected get_blueprints with dict

--- aoc_2022_python/19_Not_Enough_Minerals/19.py
+++ aoc_2022_python/19_Not_Enough_Minerals/19.py
@@ -1,16 +1,25 @@
 INPUT = "test_input"
+TIME_LIMIT = 24
+
+
+def get_max_obsidian(blueprint, robots, resources, t):
+    # начать строить роботов
+    # получить ресурсы
+    # получить роботов
+    pass
 
 
 def get_blueprints():
     def get_numbers_from_string(string: str):
-        return list(filter(lambda x: x.isnumeric(), string.split()))
+        return list(map(int, (filter(lambda x: x.isnumeric(), string.split()))))
 
     def make_blueprint(lst):
-        ore_robot = (lst[0], 0, 0)
-        clay_robot = (lst[1], 0, 0)
-        obsidian_robot = (lst[2], lst[3], 0)
-        geode_robot = (lst[4], 0, lst[5])
-        return ore_robot, clay_robot, obsidian_robot, geode_robot
+        return {
+            "ore_robot": (lst[0], 0, 0),
+            "clay_robot": (lst[1], 0, 0),
+            "obsidian_robot": (lst[2], lst[3], 0),
+            "geode_robot": (lst[4], 0, lst[5]),
+        }
 
     with open(INPUT) as file:
         data = file.readlines()
