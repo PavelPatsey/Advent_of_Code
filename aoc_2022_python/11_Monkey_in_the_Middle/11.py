632affbc241f70ea8305a9aaab04e2f317107936
Павел <patsey.pavel@gmail.com>
2023-11-22 17:02:14 +0300

    solving day 11 part 1

--- aoc_2022_python/11_Monkey_in_the_Middle/11.py
+++ aoc_2022_python/11_Monkey_in_the_Middle/11.py
@@ -1,19 +1,22 @@
 import re
+from collections import deque
 
 INPUT = "test_input"
+ROUNDS_NUMBER = 20
 
 
 def get_monkeys():
     def _make_monkey(string: str):
         data = string.strip().split("\n")
         monkey = {
-            "items": list(map(int, (re.findall(r"\b\d+\b", data[1])))),
+            "items": deque(map(int, (re.findall(r"\b\d+\b", data[1])))),
             "operation": data[2].split("Operation: new = ")[-1],
             "divisible_by": int(data[3].split("divisible by ")[-1]),
             "condition": (
-                int(data[4].split("throw to monkey ")[-1]),
                 int(data[5].split("throw to monkey ")[-1]),
+                int(data[4].split("throw to monkey ")[-1]),
             ),
+            "inspections_number": 0,
         }
         return monkey
 
@@ -23,9 +26,25 @@ def get_monkeys():
     return monkeys
 
 
+def get_answer_1(monkeys):
+    def _get_item_after_operation(item: int, operation: str) -> int:
+        pass
+
+    for i in range(ROUNDS_NUMBER):
+        for monkey in monkeys:
+            item = monkey["items"].pop()
+            item = _get_item_after_operation(item, monkey["operation"])
+            item = item // 3
+            condition = item % monkey["divisible_by"] == 0
+            to_monkey = monkey["condition"][condition]
+            monkeys[to_monkey]["items"].append(item)
+        for monkey in monkeys:
+            monkey["inspections_number"] += len(monkey["items"])
+
+
 def main():
     monkeys = get_monkeys()
-    print(monkeys)
+    print(get_answer_1(monkeys))
 
 
 if __name__ == "__main__":
