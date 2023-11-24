b834e99577a1f7c473e29723402000714c566846
Павел <patsey.pavel@gmail.com>
2023-11-24 17:39:13 +0300

    added is_int and is_lsit functions

--- aoc_2022_python/13_Distress_Signal/13.py
+++ aoc_2022_python/13_Distress_Signal/13.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 INPUT = "test_input"
 
 
@@ -8,10 +10,41 @@ def get_packets():
     return packets
 
 
+def is_int(x):
+    return isinstance(x, int)
+
+
+def is_list(x):
+    return isinstance(x, list)
+
+
+def is_right_ordered(a, b):
+    if is_list(a) and is_int(b):
+        return is_right_ordered(deepcopy(a), [b])
+    if is_int(a) and is_list(b):
+        return is_right_ordered([a], deepcopy(b))
+    if is_int(a) and is_int(b):
+        return a <= b
+    if len(a) > len(b):
+        return False
+    lst = []
+    z = list(zip(a, b))
+    for new_a, new_b in zip(a, b):
+        lst.append(is_right_ordered(new_a, new_b))
+    return all(lst)
+
+
 def main():
     packets = get_packets()
     print(packets)
+    lst = list(map(lambda x: is_right_ordered(*x), packets))
+    print(list(enumerate(lst)))
 
 
 if __name__ == "__main__":
+    assert is_right_ordered([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) is True
+    # assert is_right_ordered([[1], [2, 3, 4]], [[1], 4]) is True
+    # assert is_right_ordered([9], [[8, 7, 6]]) is False
+    # assert is_right_ordered([7, 7, 7, 7], [7, 7, 7]) is False
+
     main()
