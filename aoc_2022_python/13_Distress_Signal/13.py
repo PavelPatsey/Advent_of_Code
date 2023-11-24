c27943c95c6c662b6e5ce01d8aa5b5127bf8a878
Павел <patsey.pavel@gmail.com>
2023-11-24 18:41:03 +0300

    solved day 13 part 1

--- aoc_2022_python/13_Distress_Signal/13.py
+++ aoc_2022_python/13_Distress_Signal/13.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-INPUT = "test_input"
+INPUT = "input"
 
 
 def get_packets():
@@ -18,33 +18,58 @@ def is_list(x):
     return isinstance(x, list)
 
 
-def is_right_ordered(a, b):
+def is_right_ordered(a, b) -> int:
     if is_list(a) and is_int(b):
         return is_right_ordered(deepcopy(a), [b])
     if is_int(a) and is_list(b):
         return is_right_ordered([a], deepcopy(b))
     if is_int(a) and is_int(b):
-        return a <= b
-    if len(a) > len(b):
-        return False
-    lst = []
-    z = list(zip(a, b))
+        if a < b:
+            return 1
+        elif a == b:
+            return 0
+        elif a > b:
+            return -1
+        else:
+            print("error!")
+
     for new_a, new_b in zip(a, b):
-        lst.append(is_right_ordered(new_a, new_b))
-    return all(lst)
+        res = is_right_ordered(new_a, new_b)
+        if res == 1:
+            return 1
+        elif res == -1:
+            return -1
+    if len(a) < len(b):
+        return 1
+    elif len(a) == len(b):
+        return 0
+    elif len(a) > len(b):
+        return -1
+    else:
+        print("error_2!")
 
 
 def main():
     packets = get_packets()
-    print(packets)
     lst = list(map(lambda x: is_right_ordered(*x), packets))
     print(list(enumerate(lst)))
+    filtered = filter(lambda x: x[1] == 1, enumerate(lst))
+    mapped = map(lambda x: x[0] + 1, filtered)
+    print(sum(mapped))
 
 
 if __name__ == "__main__":
-    assert is_right_ordered([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) is True
-    # assert is_right_ordered([[1], [2, 3, 4]], [[1], 4]) is True
-    # assert is_right_ordered([9], [[8, 7, 6]]) is False
-    # assert is_right_ordered([7, 7, 7, 7], [7, 7, 7]) is False
+    assert is_right_ordered([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) == 1
+    assert is_right_ordered([[1], [2, 3, 4]], [[1], 4]) == 1
+    assert is_right_ordered([9], [[8, 7, 6]]) == -1
+    assert is_right_ordered([[4, 4], 4, 4], [[4, 4], 4, 4, 4]) == 1
+    assert is_right_ordered([7, 7, 7, 7], [7, 7, 7]) == -1
+    assert is_right_ordered([[[]]], [[]]) == -1
+    assert (
+        is_right_ordered(
+            [1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]
+        )
+        == -1
+    )
 
     main()
