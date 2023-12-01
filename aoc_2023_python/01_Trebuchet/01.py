4a21158a3c80cf454f0e6d49b1199fc035b5c4d9
Павел Пацей <patsey.pavel@gmail.com>
2023-12-01 09:04:33 +0300

    solved day 1 part 2

--- aoc_2023_python/01_Trebuchet/01.py
+++ aoc_2023_python/01_Trebuchet/01.py
@@ -1,5 +1,17 @@
 INPUT = "input"
 
+NUMERIC_DICT = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 
 def get_document():
     with open(INPUT, "r") as file:
@@ -17,9 +29,25 @@ def get_answer_1(document):
     return sum(result)
 
 
+def get_answer_2(document):
+    digits = []
+    for line in document:
+        digits_from_line = []
+        for index, char in enumerate(line):
+            if char.isnumeric():
+                digits_from_line.append(char)
+            for key, value in NUMERIC_DICT.items():
+                if line[index:].startswith(key):
+                    digits_from_line.append(NUMERIC_DICT[key])
+        digits.append(digits_from_line)
+    digits = [int(digit[0] + digit[-1]) for digit in digits]
+    return sum(digits)
+
+
 def main():
     document = get_document()
     print(get_answer_1(document))
+    print(get_answer_2(document))
 
 
 if __name__ == "__main__":
