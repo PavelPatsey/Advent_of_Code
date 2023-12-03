6ab0259531cb9134e061ffa380746c28076773bd
Павел Пацей <patsey.pavel@gmail.com>
2023-12-03 08:24:57 +0300

    added get_schema

--- aoc_2023_python/03_Gear_Ratios/03.py
+++ aoc_2023_python/03_Gear_Ratios/03.py
@@ -2,7 +2,15 @@ INPUT = "test_input"
 
 
 def get_schema():
-    pass
+    with open(INPUT, "r") as file:
+        data = file.readlines()
+    first_last_string = ["." * (len(data[0]) + 1)]
+    schema = (
+        first_last_string
+        + list(map(lambda x: "." + x[:-1] + ".", data))
+        + first_last_string
+    )
+    return schema
 
 
 def main():
