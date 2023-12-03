f9c51bde0492f52d4d1121ff87fe591822d3755a
Павел Пацей <patsey.pavel@gmail.com>
2023-12-03 09:06:36 +0300

    solved day 03 part 1

--- aoc_2023_python/03_Gear_Ratios/03.py
+++ aoc_2023_python/03_Gear_Ratios/03.py
@@ -1,4 +1,4 @@
-INPUT = "test_input"
+INPUT = "input"
 
 
 def get_schemas():
@@ -13,15 +13,52 @@ def get_schemas():
     return schemas
 
 
+def is_adjacent_to_symbol(schemas, i, j):
+    lst = [
+        schemas[i + 1][j],
+        schemas[i + 1][j + 1],
+        schemas[i][j + 1],
+        schemas[i - 1][j + 1],
+        schemas[i - 1][j],
+        schemas[i - 1][j - 1],
+        schemas[i][j - 1],
+        schemas[i + 1][j - 1],
+    ]
+    return any(map(lambda x: not x.isdigit() and x != ".", lst))
+
+
 def get_answer_1(schemas):
-    pass
+    adjacent_numbers = []
+    len_string = len(schemas[0])
+    i = 1
+    while i <= len(schemas) - 2:
+        j = 0
+        number = []
+        while j <= len_string - 1:
+            if schemas[i][j].isdigit():
+                number.append(j)
+            else:
+                if any(map(lambda x: is_adjacent_to_symbol(schemas, i, x), number)):
+                    lst = [schemas[i][n] for n in number]
+                    adjacent_numbers.append(int("".join(lst)))
+                number = []
+            j += 1
+        i += 1
+    return sum(adjacent_numbers)
 
 
 def main():
     schemas = get_schemas()
-    print(schemas)
     print(get_answer_1(schemas))
 
 
 if __name__ == "__main__":
+    schemas = [".....", "..1..", "....."]
+    assert is_adjacent_to_symbol(schemas, 1, 2) is False
+
+    schemas = [".....", "..1@.", "....."]
+    assert is_adjacent_to_symbol(schemas, 1, 2) is True
+
+    schemas = [".....", "..1..", ".%..."]
+    assert is_adjacent_to_symbol(schemas, 1, 2) is True
     main()
