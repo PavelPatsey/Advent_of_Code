dac16f1c0bc04312baaf4652e8b473514c486e46
Павел Пацей <patsey.pavel@gmail.com>
2023-12-03 12:34:48 +0300

    reworked get_answer_2

--- aoc_2023_python/03_Gear_Ratios/03.py
+++ aoc_2023_python/03_Gear_Ratios/03.py
@@ -13,6 +13,10 @@ def get_schemas():
     return schemas
 
 
+def get_adjacent_coordinates(i, j):
+    return [(i + x, j + y) for y in [-1, 0, 1] for x in [-1, 0, 1] if (x, y) != (0, 0)]
+
+
 def is_adjacent_to_symbol(schemas, i, j):
     indexes = [
         (i + x, j + y) for y in [-1, 0, 1] for x in [-1, 0, 1] if (x, y) != (0, 0)
@@ -43,11 +47,6 @@ def get_answer_1(schemas):
     return sum(adjacent_numbers)
 
 
-def get_adjacent_gears_coordinates(schemas, i, j):
-    indexes = [(x, y) for y in [-1, 0, 1] for x in [-1, 0, 1] if (x, y) != (0, 0)]
-    return [(i + di, j + dj) for di, dj in indexes if schemas[i + di][j + dj] == "*"]
-
-
 def get_answer_2(schemas):
     gear_dict = dict()
     len_string = len(schemas[0])
@@ -59,12 +58,12 @@ def get_answer_2(schemas):
             if schemas[i][j].isdigit():
                 number_indexes.append(j)
             elif number_indexes:
-                gears_coordinates = []
+                coordinates = []
                 for n in number_indexes:
-                    coordinates = get_adjacent_gears_coordinates(schemas, i, n)
-                    for coordinate in coordinates:
-                        if coordinate not in gears_coordinates:
-                            gears_coordinates.append(coordinate)
+                    coordinates += get_adjacent_coordinates(i, n)
+                gears_coordinates = [
+                    (x, y) for x, y in set(coordinates) if schemas[x][y] == "*"
+                ]
                 if gears_coordinates:
                     number_int = int("".join([schemas[i][n] for n in number_indexes]))
                     for coordinate in gears_coordinates:
@@ -98,19 +97,5 @@ if __name__ == "__main__":
 
     schemas = [".....", "..1..", ".%..."]
     assert is_adjacent_to_symbol(schemas, 1, 2) is True
-
-    schemas = [
-        ".....",
-        "..1..",
-        ".*...",
-    ]
-    assert get_adjacent_gears_coordinates(schemas, 1, 2) == [(2, 1)]
-
-    schemas = [
-        ".....",
-        "..1..",
-        ".@...",
-    ]
-    assert get_adjacent_gears_coordinates(schemas, 1, 2) == []
 
     main()
