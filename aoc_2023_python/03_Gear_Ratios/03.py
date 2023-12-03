17a5e9b83c9c4d9fbb5ada158f76aa5ca16d3983
Павел Пацей <patsey.pavel@gmail.com>
2023-12-03 11:21:45 +0300

    solved test part 2

--- aoc_2023_python/03_Gear_Ratios/03.py
+++ aoc_2023_python/03_Gear_Ratios/03.py
@@ -1,4 +1,4 @@
-INPUT = "input"
+INPUT = "test_input"
 
 
 def get_schemas():
@@ -33,23 +33,72 @@ def get_answer_1(schemas):
     i = 1
     while i <= len(schemas) - 2:
         j = 0
-        number = []
+        number_indexes = []
         while j <= len_string - 1:
             if schemas[i][j].isdigit():
-                number.append(j)
+                number_indexes.append(j)
             else:
-                if any(map(lambda x: is_adjacent_to_symbol(schemas, i, x), number)):
-                    lst = [schemas[i][n] for n in number]
+                if any(
+                    map(lambda x: is_adjacent_to_symbol(schemas, i, x), number_indexes)
+                ):
+                    lst = [schemas[i][n] for n in number_indexes]
                     adjacent_numbers.append(int("".join(lst)))
-                number = []
+                number_indexes = []
             j += 1
         i += 1
     return sum(adjacent_numbers)
 
 
+def get_adjacent_gears_coordinates(schemas, i, j):
+    indexes = [(0, 1), (0, -1), (1, 1), (1, -1), (-1, -1), (-1, 1)]
+    return [(i + di, j + dj) for di, dj in indexes if schemas[i + di][j + dj] == "*"]
+
+
+def get_answer_2(schemas):
+    gear_dict = dict()
+    len_string = len(schemas[0])
+    i = 1
+    while i <= len(schemas) - 2:
+        j = 0
+        number_indexes = []
+        while j <= len_string - 1:
+            if schemas[i][j].isdigit():
+                number_indexes.append(j)
+            else:
+                gears_coordinates = []
+                for n in number_indexes:
+                    coordinates = get_adjacent_gears_coordinates(schemas, i, n)
+                    for coordinate in coordinates:
+                        if coordinate not in gears_coordinates:
+                            gears_coordinates.append(coordinate)
+                if gears_coordinates:
+                    number_int = int("".join([schemas[i][n] for n in number_indexes]))
+                    for coordinate in gears_coordinates:
+                        if coordinate in gear_dict.keys():
+                            gear_dict[coordinate].append(number_int)
+                        else:
+                            gear_dict[coordinate] = [number_int]
+                number_indexes = []
+            j += 1
+        i += 1
+
+    result_list = []
+    result = 0
+    from pprint import pprint
+
+    pprint(len(gear_dict))
+    for value in gear_dict.values():
+        if len(value) == 2:
+            result = result + value[0] * value[1]
+            result_list.append(value[0] * value[1])
+    print(sorted(result_list))
+    return result
+
+
 def main():
     schemas = get_schemas()
     print(get_answer_1(schemas))
+    print(get_answer_2(schemas))
 
 
 if __name__ == "__main__":
@@ -61,4 +110,19 @@ if __name__ == "__main__":
 
     schemas = [".....", "..1..", ".%..."]
     assert is_adjacent_to_symbol(schemas, 1, 2) is True
+
+    schemas = [
+        ".....",
+        "..1..",
+        ".*...",
+    ]
+    assert get_adjacent_gears_coordinates(schemas, 1, 2) == [(2, 1)]
+
+    schemas = [
+        ".....",
+        "..1..",
+        ".@...",
+    ]
+    assert get_adjacent_gears_coordinates(schemas, 1, 2) == []
+
     main()
