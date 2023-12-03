85c4413edee6c0a1efa108cf931a426f0b387011
Павел Пацей <patsey.pavel@gmail.com>
2023-12-03 08:26:35 +0300

    cosmetic changes

--- aoc_2023_python/03_Gear_Ratios/03.py
+++ aoc_2023_python/03_Gear_Ratios/03.py
@@ -1,21 +1,26 @@
 INPUT = "test_input"
 
 
-def get_schema():
+def get_schemas():
     with open(INPUT, "r") as file:
         data = file.readlines()
     first_last_string = ["." * (len(data[0]) + 1)]
-    schema = (
+    schemas = (
         first_last_string
         + list(map(lambda x: "." + x[:-1] + ".", data))
         + first_last_string
     )
-    return schema
+    return schemas
+
+
+def get_answer_1(schemas):
+    pass
 
 
 def main():
-    schema = get_schema()
-    print(schema)
+    schemas = get_schemas()
+    print(schemas)
+    print(get_answer_1(schemas))
 
 
 if __name__ == "__main__":
