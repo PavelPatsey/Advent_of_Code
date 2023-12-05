5c826a17a3b552a5f9ff419d0cd99380a2187f6e
Павел Пацей <patsey.pavel@gmail.com>
2023-12-05 09:10:05 +0300

    added get_seed_almanac

---
+++ aoc_2023_python/05_If_You_Give_A_Seed_A_Fertilizer/05.py
@@ -0,0 +1,29 @@
+INPUT = "test_input"
+
+
+def get_seed_almanac():
+    with open(INPUT, "r") as file:
+        data = file.read()
+    lst = data.strip().split("\n\n")
+    seeds, almanac = lst[0], lst[1:]
+    seeds = [int(x) for x in seeds.split(":")[1].strip().split(" ")]
+    almanac_dict = {}
+    for x in almanac:
+        key, lst = x.strip().split(" map:\n")
+        value = [[int(y) for y in x.strip().split()] for x in lst.split("\n")]
+        almanac_dict[key] = value
+    return seeds, almanac_dict
+
+
+def get_answer_1(almanac):
+    pass
+
+
+def main():
+    seeds, almanac = get_seed_almanac()
+    print(seeds)
+    print(almanac)
+
+
+if __name__ == "__main__":
+    main()
