4e5b0fdc7d7408211ffbaf54f5a38a918871f03e
Павел Пацей <patsey.pavel@gmail.com>
2023-12-05 08:22:20 +0300

    start day 05

---
+++ aoc_2023_python/05_/05.py
@@ -0,0 +1,18 @@
+INPUT = "input"
+
+
+def get_cards():
+    with open(INPUT, "r") as file:
+        data = file.readlines()
+
+
+def get_answer_1(cards):
+    pass
+
+
+def main():
+    cards = get_cards()
+
+
+if __name__ == "__main__":
+    main()
