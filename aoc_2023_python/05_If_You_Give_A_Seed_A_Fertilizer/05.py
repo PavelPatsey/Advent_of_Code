c9f03dd6aea4b5446f11686e4aaf6f66ed3a1fe6
Павел Пацей <patsey.pavel@gmail.com>
2023-12-05 11:11:51 +0300

    reworked get_answer_1

--- aoc_2023_python/05_If_You_Give_A_Seed_A_Fertilizer/05.py
+++ aoc_2023_python/05_If_You_Give_A_Seed_A_Fertilizer/05.py
@@ -1,4 +1,4 @@
-INPUT = "input"
+INPUT = "test_input"
 
 
 def get_seed_almanac():
@@ -16,35 +16,24 @@ def get_seed_almanac():
 
 
 def get_answer_1(seeds, almanac):
-    match_list = [[seed] for seed in seeds]
-    for i in range(len(seeds)):
-        for almanac_value in almanac.values():
-            is_found = False
-            for destination, source, range_length in almanac_value:
-                if match_list[i][-1] in range(source, source + range_length):
-                    match_list[i].append(match_list[i][-1] + destination - source)
-                    is_found = True
-                    break
-            if not is_found:
-                match_list[i].append(match_list[i][-1])
-    return min([x[-1] for x in match_list])
-
-
-def get_answer_2(seeds, almanac):
-    min_location = 1_0000_0000000
+    min_location = None
     for i in range(len(seeds)):
         match_list = [seeds[i]]
         for almanac_value in almanac.values():
             is_found = False
             for destination, source, range_length in almanac_value:
-                if match_list[-1] in range(source, source + range_length):
+                if source <= match_list[-1] <= source + range_length - 1:
                     match_list.append(match_list[-1] + destination - source)
+                    del match_list[0]
                     is_found = True
                     break
             if not is_found:
                 match_list.append(match_list[-1])
-        min_location = min(match_list[-1], min_location)
-        print(min_location)
+                del match_list[0]
+        if not min_location:
+            min_location = match_list[-1]
+        else:
+            min_location = min(match_list[-1], min_location)
     return min_location
 
 
@@ -53,10 +42,6 @@ def main():
     print(seeds)
     print(almanac)
     print(get_answer_1(seeds, almanac))
-    print(get_answer_2(seeds, almanac))
-    seeds_2 = list(range(seeds[0], seeds[0] + seeds[1])) + list(range(seeds[2], seeds[2] + seeds[3]))
-    print(seeds_2)
-    print(get_answer_2(seeds_2, almanac))
 
 
 if __name__ == "__main__":
