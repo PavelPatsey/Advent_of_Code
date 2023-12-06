fb07c0a5a73de3be56ee0b95029dd3b54ef88c63
Павел <patsey.pavel@gmail.com>
2023-12-06 16:57:22 +0300

    solved day 06 part 1

--- aoc_2023_python/06_Wait_For_It/06.py
+++ aoc_2023_python/06_Wait_For_It/06.py
@@ -1,4 +1,6 @@
-INPUT = "test_input"
+from functools import reduce
+
+INPUT = "input"
 
 
 def get_races():
@@ -9,9 +11,26 @@ def get_races():
     return races
 
 
+def get_farther_distances(race):
+    time, max_distance = race
+    farther_distances = []
+    for t in range(time + 1):
+        v = t
+        distance = v * (time - t)
+        if distance > max_distance:
+            farther_distances.append(distance)
+    return farther_distances
+
+
+def get_answer_1(races):
+    farther_distances = map(get_farther_distances, races)
+    lens = map(len, farther_distances)
+    return reduce((lambda x, y: x * y), lens, 1)
+
+
 def main():
     races = get_races()
-    print(races)
+    print(get_answer_1(races))
 
 
 if __name__ == "__main__":
