ef07b2a27fba6e837b3106a3d1d2e8965be9f43f
Павел Пацей <patsey.pavel@gmail.com>
2023-12-09 08:36:31 +0300

    solved test day 09 part 1

--- aoc_2023_python/09_Mirage_Maintenance/09.py
+++ aoc_2023_python/09_Mirage_Maintenance/09.py
@@ -5,10 +5,37 @@ def get_history(inp):
     return history
 
 
+def get_predicted_value(sequence):
+    """
+    0   3   6   9  12  15  18
+      3   3   3   3   3   3
+        0   0   0   0   0
+    """
+
+    def _is_all_null(seq):
+        return all(map(lambda x: x == 0, seq))
+
+    sequences = [sequence]
+    while not _is_all_null(sequences[-1]):
+        previous_seq = sequences[-1]
+        new_seq = [y - x for x, y in zip(previous_seq, previous_seq[1:])]
+        sequences.append(new_seq)
+
+    return sum(map(lambda x: x[-1], sequences))
+
+
+def get_answer_1(history):
+    return sum(map(get_predicted_value, history))
+
+
 def main():
     history = get_history("test_input")
     print(history)
+    print(get_answer_1(history))
 
 
 if __name__ == "__main__":
+    assert get_predicted_value([0, 3, 6, 9, 12, 15]) == 18
+    assert get_predicted_value([1, 3, 6, 10, 15, 21]) == 28
+    assert get_predicted_value([10, 13, 16, 21, 30, 45]) == 68
     main()
