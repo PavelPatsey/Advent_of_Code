cd1d849b518509f6aa57a8b711f961d02c20330c
Павел Пацей <patsey.pavel@gmail.com>
2023-12-09 09:16:49 +0300

    get_predicted_value depends on argument part

--- aoc_2023_python/09_Mirage_Maintenance/09.py
+++ aoc_2023_python/09_Mirage_Maintenance/09.py
@@ -5,7 +5,7 @@ def get_history(inp):
     return history
 
 
-def get_predicted_value(sequence):
+def get_predicted_value(sequence, part):
     def _is_all_null(seq):
         return all(map(lambda x: x == 0, seq))
 
@@ -15,31 +15,21 @@ def get_predicted_value(sequence):
         new_seq = [y - x for x, y in zip(previous_seq, previous_seq[1:])]
         sequences.append(new_seq)
 
-    return sum(map(lambda x: x[-1], sequences))
-
-
-def get_predicted_previous_value(sequence):
-    def _is_all_null(seq):
-        return all(map(lambda x: x == 0, seq))
-
-    sequences = [sequence]
-    while not _is_all_null(sequences[-1]):
-        previous_seq = sequences[-1]
-        new_seq = [y - x for x, y in zip(previous_seq, previous_seq[1:])]
-        sequences.append(new_seq)
-
-    result = 0
-    for i in reversed(range(1, len(sequences))):
-        result = sequences[i - 1][0] - result
-    return result
+    if part == 1:
+        return sum(map(lambda x: x[-1], sequences))
+    else:
+        result = 0
+        for i in reversed(range(1, len(sequences))):
+            result = sequences[i - 1][0] - result
+        return result
 
 
 def get_answer_1(history):
-    return sum(map(get_predicted_value, history))
+    return sum(map(lambda x: get_predicted_value(x, part=1), history))
 
 
 def get_answer_2(history):
-    return sum(map(get_predicted_previous_value, history))
+    return sum(map(lambda x: get_predicted_value(x, part=2), history))
 
 
 def main():
@@ -49,11 +39,11 @@ def main():
 
 
 if __name__ == "__main__":
-    assert get_predicted_value([0, 3, 6, 9, 12, 15]) == 18
-    assert get_predicted_value([1, 3, 6, 10, 15, 21]) == 28
-    assert get_predicted_value([10, 13, 16, 21, 30, 45]) == 68
+    assert get_predicted_value([0, 3, 6, 9, 12, 15], 1) == 18
+    assert get_predicted_value([1, 3, 6, 10, 15, 21], 1) == 28
+    assert get_predicted_value([10, 13, 16, 21, 30, 45], 1) == 68
 
-    assert get_predicted_previous_value([0, 3, 6, 9, 12, 15]) == -3
-    assert get_predicted_previous_value([1, 3, 6, 10, 15, 21]) == 0
-    assert get_predicted_previous_value([10, 13, 16, 21, 30, 45]) == 5
+    assert get_predicted_value([0, 3, 6, 9, 12, 15], 2) == -3
+    assert get_predicted_value([1, 3, 6, 10, 15, 21], 2) == 0
+    assert get_predicted_value([10, 13, 16, 21, 30, 45], 2) == 5
     main()
