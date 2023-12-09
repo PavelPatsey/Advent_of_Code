85429f1772790232d5d2752a1109de7ef8dd836b
Павел Пацей <patsey.pavel@gmail.com>
2023-12-09 08:21:56 +0300

    added parsing

--- aoc_2023_python/09_Mirage_Maintenance/09.py
+++ aoc_2023_python/09_Mirage_Maintenance/09.py
@@ -1,5 +1,13 @@
+def get_history(inp):
+    with open(inp, "r") as file:
+        data = file.readlines()
+    history = list(map(lambda x: [int(y) for y in x.strip().split()], data))
+    return history
+
+
 def main():
-    print("start")
+    history = get_history("test_input")
+    print(history)
 
 
 if __name__ == "__main__":
