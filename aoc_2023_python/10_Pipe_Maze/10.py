201738fe894871ef37cb8474d18f9bb04bbf06d8
Павел <patsey.pavel@gmail.com>
2023-12-10 16:58:38 +0300

    solved day 10 part 1

--- aoc_2023_python/10_Pipe_Maze/10.py
+++ aoc_2023_python/10_Pipe_Maze/10.py
@@ -6,7 +6,19 @@ RIGHT = (0, 1)
 UP = (-1, 0)
 DOWN = (1, 0)
 
-TRENDS = [LEFT, RIGHT, UP, DOWN]
+DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
+
+# старое направление -> node -> новое направление
+# направо -> J -> наверх
+NEXT_DIR_DICT = {
+    ".": [None, None, None, None],
+    "J": [None, 2, None, 0],
+    "L": [2, None, None, 1],
+    "F": [3, None, 1, None],
+    "7": [None, 3, 0, None],
+    "-": [0, 1, None, None],
+    "|": [None, None, 2, 3],
+}
 
 
 def get_nodes(input_file):
@@ -14,57 +26,40 @@ def get_nodes(input_file):
         data = file.readlines()
     first_last_string = ["." * (len(data[0]) + 1)]
     pipes = (
-            first_last_string
-            + list(map(lambda x: "." + x[:-1] + ".", data))
-            + first_last_string
+        first_last_string
+        + list(map(lambda x: "." + x[:-1] + ".", data))
+        + first_last_string
     )
     return pipes
 
 
-def get_next_trend(current_node: str, previous_trend):
-    if current_node == ".":
-        return
-
-    right_nodes = ["-", "7", "J"]
-    left_nodes = ["-", "L", "F"]
-    up_nodes = ["|", "7", "F"]
-    down_nodes = ["|", "L", "J"]
-
-    D_1 = {
-        RIGHT: right_nodes,
-        LEFT: left_nodes,
-        UP: up_nodes,
-        DOWN: down_nodes,
-    }
-
-    elif current_node == "F":
-    if previous_trend == UP:
-        return RIGHT
-    elif previous_trend == LEFT:
-        return DOWN
-    else:
-        return
+def get_next_direction(i, j, nodes, prev_dir_index):
+    current_node = nodes[i][j]
+    assert current_node != "S"
+    new_dir_index = NEXT_DIR_DICT[current_node][prev_dir_index]
+    return new_dir_index
 
 
-next_trend = (0, 1)
-return next_trend
-
-
-def get_visited(start_i, start_j, nodes, trend):
-    node = nodes[start_i, start_j]
-    visited = [node]
-
-    i = start_i
-    j = start_j
+def get_cycle_len(start_i, start_j, nodes, dir_index):
+    counter = 1
+    di, dj = DIRECTIONS[dir_index]
+    i, j = start_i + di, start_j + dj
+    node = nodes[i][j]
 
     while node != "S":
-        di, dj = get_next_trend()
-        new_i = i + di
-        new_j = j + dj
-        node = nodes[new_i][new_j]
-        visited.append(node)
+        n = node
+        prev = dir_index
+        dir_index = get_next_direction(i, j, nodes, dir_index)
+        next = dir_index
+        if dir_index is None:
+            return
+        di, dj = DIRECTIONS[dir_index]
+        i = i + di
+        j = j + dj
+        node = nodes[i][j]
+        counter += 1
 
-    return visited
+    return counter
 
 
 def get_start_coordinates(nodes):
@@ -78,8 +73,10 @@ def get_start_coordinates(nodes):
 
 def get_answer_1(nodes):
     x, y = get_start_coordinates(nodes)
-    visited_lists = [get_visited(x, y, nodes, trend) for trend in TRENDS]
-    return max(map(len, visited_lists))
+    for i in range(len(DIRECTIONS)):
+        length = get_cycle_len(x, y, nodes, i)
+        if length is not None:
+            return length // 2
 
 
 def main():
