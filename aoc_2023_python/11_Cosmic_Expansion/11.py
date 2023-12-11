3fd18fbdbb3f7c4fc0f639f17a54c6e46f1fbf56
Павел Пацей <patsey.pavel@gmail.com>
2023-12-11 09:03:55 +0300

    solved test part 1 day 11

--- aoc_2023_python/11_Cosmic_Expansion/11.py
+++ aoc_2023_python/11_Cosmic_Expansion/11.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def get_space(input_file):
     with open(input_file, "r") as file:
         data = file.readlines()
@@ -40,8 +43,28 @@ def get_galaxies_coordinates(space):
 
 
 def get_answer_1(space):
-    galaxies_coordinates = get_galaxies_coordinates(space)
+    galaxies_coordinates = deque(get_galaxies_coordinates(space))
+    print(galaxies_coordinates)
     empty_rows_indexes, empty_columns_indexes = get_empty_lines_indexes(space)
+    print(empty_rows_indexes, empty_columns_indexes)
+    shortest_paths = []
+    while galaxies_coordinates:
+        current_g_c = galaxies_coordinates.popleft()
+        for g_c in galaxies_coordinates:
+            r1, r2 = sorted((current_g_c[0], g_c[0]))
+            c1, c2 = sorted((current_g_c[1], g_c[1]))
+            dr = r2 - r1
+            dc = c2 - c1
+            for r in range(r1, r2 + 1):
+                if r in empty_rows_indexes:
+                    dr += 1
+            for c in range(c1, c2 + 1):
+                if c in empty_columns_indexes:
+                    dc += 1
+            shortest_paths.append(dr + dc)
+    print(shortest_paths)
+    print(sum(shortest_paths))
+    return sum(shortest_paths)
 
 
 def main():
