544ddd4dbcdcfb2d284af3068e3f8320a2bc80e8
Павел Пацей <patsey.pavel@gmail.com>
2023-12-13 09:37:44 +0300

    added function get_amount for columns case

--- aoc_2023_python/13_Point_of_Incidence/13.py
+++ aoc_2023_python/13_Point_of_Incidence/13.py
@@ -7,9 +7,30 @@ def get_mirrors(input_file):
 
 
 def get_amount(mirror):
-    # len_rows = len(mirror)
-    # len_cols = len(mirror[0])
-    pass
+    len_rows = len(mirror)
+    len_cols = len(mirror[0])
+    c = 0
+    line_is_found = False
+    while c < len_cols and not line_is_found:
+        dc = 0
+        left = c - dc
+        right = c + dc + 1
+        condition = 0 <= left < right < len_cols
+        is_break = False
+        while condition and not is_break:
+            is_break = False
+            for r in range(len_rows):
+                if mirror[r][left] != mirror[r][right]:
+                    is_break = True
+                    break
+            dc += 1
+            left = c - dc
+            right = c + dc + 1
+            condition = 0 <= left < right < len_cols
+        c += 1
+        if not is_break:
+            line_is_found = True
+    return c
 
 
 def get_answer_1(mirrors):
@@ -35,5 +56,4 @@ if __name__ == "__main__":
     ]
     assert get_amount(mirror) == 5
 
-    main()
-v
+    # main()
