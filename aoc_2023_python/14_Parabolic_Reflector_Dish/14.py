5d2fe01405780b6cb97a1675f0043bd57b23560c
Павел Пацей <patsey.pavel@gmail.com>
2023-12-14 11:30:42 +0300

    solved day 14 part 2

--- aoc_2023_python/14_Parabolic_Reflector_Dish/14.py
+++ aoc_2023_python/14_Parabolic_Reflector_Dish/14.py
@@ -1,3 +1,4 @@
+import time
 from copy import deepcopy
 
 
@@ -26,25 +27,49 @@ def get_rolled(matrix):
     return platform
 
 
-def get_total_load(platform):
+def get_load(platform):
     len_rows = len(platform)
     len_cols = len(platform[0])
     total_load = 0
     for c in range(len_cols):
-        current_load = len_rows
         for r in range(len_rows):
             if platform[r][c] == "O":
-                total_load += current_load
-                current_load -= 1
-            elif platform[r][c] == "#":
-                current_load = len_rows - r - 1
+                total_load += len_rows - r
+
     return total_load
 
 
+def get_answer_1(platform):
+    return get_load(get_rolled(platform))
+
+
+def get_answer_2(platform):
+    cache_dict = {}
+    max_t = 10**9
+    t = 0
+    while t < max_t:
+        t += 1
+        for j in range(4):
+            platform = get_rolled(platform)
+            platform = get_rotated(platform)
+        platform_tuple = tuple(tuple(row) for row in platform)
+        if platform_tuple in cache_dict:
+            cycle_length = t - cache_dict[platform_tuple]
+            cycle_count = (max_t - t) // cycle_length
+            t += cycle_count * cycle_length
+        cache_dict[platform_tuple] = t
+    return get_load(platform)
+
+
 def main():
-    platform = get_platform("test_input")
-    print(platform)
-    print(get_total_load(platform))
+    t0 = time.time()
+    platform = get_platform("input")
+    print(get_answer_1(platform))
+    print(f"finished in {time.time() - t0:0f} sec")
+
+    t0 = time.time()
+    print(get_answer_2(platform))
+    print(f"finished in {time.time() - t0:0f} sec")
 
 
 if __name__ == "__main__":
