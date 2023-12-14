1cc87fdb39b4857e4dc71c72cda27817a24a6b76
Павел Пацей <patsey.pavel@gmail.com>
2023-12-14 09:32:52 +0300

    added get_rotated

--- aoc_2023_python/14_Parabolic_Reflector_Dish/14.py
+++ aoc_2023_python/14_Parabolic_Reflector_Dish/14.py
@@ -5,6 +5,11 @@ def get_platform(input_file):
     return platform
 
 
+def get_rotated(matrix):
+    list_of_tuples = zip(*matrix[::-1])
+    return [list(elem) for elem in list_of_tuples]
+
+
 def get_total_load(platform):
     len_rows = len(platform)
     len_cols = len(platform[0])
@@ -22,9 +27,19 @@ def get_total_load(platform):
 
 def main():
     platform = get_platform("test_input")
-    print(platform)
     print(get_total_load(platform))
 
 
 if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    assert get_rotated(matrix) == [
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3],
+    ]
+
     main()
