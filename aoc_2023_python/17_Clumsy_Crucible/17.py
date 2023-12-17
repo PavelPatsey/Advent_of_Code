99154b4a7ca2995fab7e03a2ce4164d7c0e64112
Павел Пацей <patsey.pavel@gmail.com>
2023-12-17 21:45:36 +0300

    part 2 with error

--- aoc_2023_python/17_Clumsy_Crucible/17.py
+++ aoc_2023_python/17_Clumsy_Crucible/17.py
@@ -23,7 +23,7 @@ def get_new_dirs_lens_1(direction, len_dir):
 
 def get_new_dirs_lens_2(direction, len_dir):
     new_dirs_lens = []
-    if len_dir > 3:
+    if len_dir >= 4:
         if direction in (RIGHT, LEFT):
             new_dirs_lens.extend([(UP, 1), (DOWN, 1)])
         elif direction in (UP, DOWN):
@@ -38,7 +38,7 @@ def get_new_dirs_lens_2(direction, len_dir):
 def get_min_heat_loss(matrix, part_2=False):
     len_rows = len(matrix)
     len_cols = len(matrix[0])
-    queue = [(0, 0, 0, RIGHT, 1)]
+    queue = [(0, 0, 0, RIGHT, 0)]
     heapq.heapify(queue)
     state = [[{} for _ in range(len_cols)] for _ in range(len_rows)]
 
@@ -68,7 +68,7 @@ def get_answer(matrix, part_2=False):
 
 
 def main():
-    matrix = get_matrix("test_input")
+    matrix = get_matrix("test_input_2")
     print(get_answer(matrix))
     print(get_answer(matrix, True))
 
