1d4fd71369a5d03306048fff6381f2afc7b1fed2
Павел Пацей <patsey.pavel@gmail.com>
2023-12-18 10:48:19 +0300

    solved day 18 part 2

--- aoc_2023_python/18_Lavaduct_Lagoon/18.py
+++ aoc_2023_python/18_Lavaduct_Lagoon/18.py
@@ -1,4 +1,5 @@
 DIR_DICT = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
+DIR_LIST = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
 
 def get_plan(input_file):
@@ -39,9 +40,31 @@ def get_answer_1(plan):
     return result
 
 
+def get_answer_2(plan):
+    points = [(0, 0)]
+    length = 0
+    for _, _, hexadecimal_code in plan:
+        n = int(hexadecimal_code[1:-1], 16)
+        dir_ = DIR_LIST[int(hexadecimal_code[-1])]
+        length += n
+        x, y = points[-1]
+        dx, dy = dir_[0] * n, dir_[1] * n
+        points.append((x + dx, y + dy))
+
+    triangle_areas = []
+    for ((x2, y2), (x3, y3)) in zip(points[1:], points[2:]):
+        triangle_areas.append(get_triangle_area(0, 0, x2, y2, x3, y3))
+
+    area = abs(sum(triangle_areas))
+    area_1 = area - length // 2 + 1
+    result = area_1 + length
+    return result
+
+
 def main():
     plan = get_plan("input")
     print(get_answer_1(plan))
+    print(get_answer_2(plan))
 
 
 if __name__ == "__main__":
