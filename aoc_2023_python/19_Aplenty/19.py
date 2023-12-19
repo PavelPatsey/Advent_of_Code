f4d0910364a278385a4d3bbdaca1c64f6135c4c3
Павел Пацей <patsey.pavel@gmail.com>
2023-12-19 09:48:18 +0300

    solved day 19 part 1

---
+++ aoc_2023_python/19_Aplenty/19.py
@@ -0,0 +1,77 @@
+def get_parsed_input(input_file):
+    with open(input_file, "r") as file:
+        data = file.read()
+    x = "x"
+    m = "m"
+    a = "a"
+    s = "s"
+    workflows, parts = data.strip().split("\n\n")
+    workflows = workflows.split()
+    workflows_dict = {}
+    for line in workflows:
+        key, value = line.split("{")
+        value = value.replace("}", "")
+        value = value.split(",")
+        # value_dict = {}
+        # for v in value:
+        #     if ":" in v:
+        #         value_dict[v[0]] = v
+        #     else:
+        #         value_dict[v] = v
+        workflows_dict[key] = value
+
+    parts = parts.split()
+    parts_list = []
+    for line in parts:
+        line = line.replace("=", ":")
+        parts_list.append(eval(line))
+
+    return workflows_dict, parts_list
+
+
+def is_accepted(part, workflows):
+    x = part["x"]
+    m = part["m"]
+    a = part["a"]
+    s = part["s"]
+
+    key = "in"
+    i = 0
+    while True:
+        if key in ("A", "R"):
+            break
+
+        value = workflows[key][i]
+
+        if ":" in value:
+            condition, go_to = value.split(":")
+            condition = eval(condition)
+            if condition:
+                key = go_to
+                i = 0
+            else:
+                i += 1
+        else:
+            key = value
+            i = 0
+    return True if key == "A" else False
+
+
+def get_answer_1(workflows, parts):
+    # print(list(map(lambda x: is_accepted(x, workflows), parts)))
+    result = 0
+    for part in parts:
+        if is_accepted(part, workflows):
+            result += sum(part.values())
+    return result
+
+
+def main():
+    workflows, parts = get_parsed_input("input")
+    print(f"{workflows=}")
+    print(f"{parts=}")
+    print(get_answer_1(workflows, parts))
+
+
+if __name__ == "__main__":
+    main()
