241f314b2764399225e4a34d3cda8ea1e382a340
Павел Пацей <patsey.pavel@gmail.com>
2023-12-20 10:37:09 +0300

    solving part 1

--- aoc_2023_python/20_Pulse_Propagation/20.py
+++ aoc_2023_python/20_Pulse_Propagation/20.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def get_config(input_file):
     with open(input_file, "r") as file:
         data = file.readlines()
@@ -5,24 +8,53 @@ def get_config(input_file):
     for line in data:
         mod, dest_mods = line.strip().split(" -> ")
         if mod == "broadcaster":
-            name = prefix = "broadcaster"
+            name = type_ = "broadcaster"
         else:
-            prefix = mod[0]
+            type_ = mod[0]
             name = mod[1:]
         dest_mods = dest_mods.split(", ")
-        config[name] = {"prefix": prefix, "dest_mods": dest_mods}
-    return config
+        config[name] = {
+            "type": type_,
+            "pulse": False,
+            "dest_mods": dest_mods,
+            "memory": [],
+        }
+        return config
 
+    def get_converted_module(module, pulse):
+        """
+        module = {"type_": type_, "pulse": False, "dest_mods": dest_mods, "memory": []}
+        """
+        if module["type"] == "broadcaster":
+            return module
+        elif module["type"] == "%":
+            pass
+        elif module["type"] == "&":
+            pass
+        else:
+            assert False
 
-def get_answer_1(config):
-    return
+    def get_answer_1(config):
+        N = 1000
+        l_p = 0
+        h_p = 0
+        for _ in range(N):
+            queue = deque(["broadcaster"])
+            while queue:
+                name = queue.popleft()
+                module = config[name]
+                pulse = module["pulse"]
+                if pulse:
+                    h_p += 1
+                else:
+                    l_p += 1
+                new_module = get_converted_module(module, pulse)
+                queue = queue + deque(new_module["dest_mods"])
 
+    def main():
+        config = get_config("test_input_1")
+        print(config)
+        print(get_answer_1(config))
 
-def main():
-    config = get_config("test_input_1")
-    print(config)
-    print(get_answer_1(config))
-
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
