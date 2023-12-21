f0b6bc0705d70e0e0b8d2d22d8fc1b9909753d41
Павел <patsey.pavel@gmail.com>
2023-12-21 16:33:18 +0300

    passed test part 1

--- aoc_2023_python/21_Step_Counter/21.py
+++ aoc_2023_python/21_Step_Counter/21.py
@@ -1,18 +1,49 @@
+DIRS_LIST = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+
+
 def get_map(input_file):
     with open(input_file, "r") as file:
         data = file.readlines()
     elfs_map = [[x for x in line.strip()] for line in data]
-    return elfs_map
+
+    start = -1, -1
+    len_rows = len(elfs_map)
+    len_cols = len(elfs_map[0])
+    for r in range(len_rows):
+        for c in range(len_cols):
+            if elfs_map[r][c] == "S":
+                start = r, c
+                break
+
+    return elfs_map, start
 
 
-def get_answer_1(elfs_map):
-    pass
+def get_answer_1(elfs_map, start, max_steps):
+    len_rows = len(elfs_map)
+    len_cols = len(elfs_map[0])
+    queue = set()
+    queue.add(start)
+    s = 0
+    while s < max_steps:
+        # neighbour
+        next_queue = set()
+        for r, c in queue:
+            for dr, dc in DIRS_LIST:
+                new_r, new_c = r + dr, c + dc
+                if 0 <= new_r < len_rows and 0 <= new_c < len_cols:
+                    if elfs_map[new_r][new_c] in (".", "S"):
+                        next_queue.add((new_r, new_c))
+        queue = next_queue
+        s += 1
+    return len(queue)
 
 
 def main():
-    elfs_map = get_map("test_input")
-    print(elfs_map)
-    print(get_answer_1(elfs_map))
+    elfs_map, start = get_map("test_input")
+    max_steps = 6
+    print(start)
+    print(start)
+    print(get_answer_1(elfs_map, start, max_steps))
 
 
 if __name__ == "__main__":
