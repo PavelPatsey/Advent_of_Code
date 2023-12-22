39b61f90f19702b5292bfde54cab67fadd4e2f69
Павел Пацей <patsey.pavel@gmail.com>
2023-12-22 10:07:36 +0300

    added the main solution algorithm

--- aoc_2023_python/22_Sand_Slabs/22.py
+++ aoc_2023_python/22_Sand_Slabs/22.py
@@ -9,8 +9,37 @@ def get_bricks(input_file):
     return [_get_parsed_line(line) for line in data]
 
 
-def get_answer_1(bricks):
-    return
+def is_may_fall(brick, other_bricks):
+    return False
+
+
+def get_on_whom_lies(index, other_bricks):
+    return set()
+
+
+def get_dropped_brick(brick):
+    start, end = brick
+    xs, ys, zs = start
+    xe, ye, ze = end
+    assert zs <= ze
+    zs = zs - 1
+    ze = ze - 1
+    return (xs, ys, zs), (xe, ye, ze)
+
+
+def get_answer_1(input_bricks):
+    sorted_bricks = sorted(input_bricks, key=lambda x: x[0][2])
+    bricks = []
+    supporting = {}
+
+    for i, brick in enumerate(sorted_bricks):
+        while not is_may_fall(brick, sorted_bricks[:i]):
+            brick = get_dropped_brick()
+        bricks.append(brick)
+        for x in get_on_whom_lies(i, sorted_bricks[:i]):
+            supporting[x].add(i)
+
+    return sum(map(lambda v: v - 1, supporting.values()))
 
 
 def main():
