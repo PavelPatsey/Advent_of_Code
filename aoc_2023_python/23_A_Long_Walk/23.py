3b684d9da2ee886d56e5f109b2ad28838311744f
Павел Пацей <patsey.pavel@gmail.com>
2023-12-23 21:17:32 +0300

    added the main solution algorithm

--- aoc_2023_python/23_A_Long_Walk/23.py
+++ aoc_2023_python/23_A_Long_Walk/23.py
@@ -4,14 +4,21 @@ def get_matrix(input_file):
     return [line.strip() for line in data]
 
 
-def get_answer_1(matrix):
-    return
+def get_max_steps(matrix):
+    def _get_max_steps(r, c, visited) -> int:
+        return 1
+
+    len_rows = len(matrix)
+    len_cols = len(matrix[0])
+    start = 0, 1
+    end = len_rows - 1, len_cols - 2
+    return _get_max_steps(0, 1, set())
 
 
 def main():
     matrix = get_matrix("test_input")
     print(matrix)
-    print(get_answer_1(matrix))
+    print(get_max_steps(matrix))
 
 
 if __name__ == "__main__":
