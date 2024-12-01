4f0d76d66a7183ba7a69cea4e54e7b9f33d7d902
Павел <patsey.pavel@gmail.com>
2024-12-01 08:48:13 +0300

    solved day 1 part 2

--- aoc_2024_python/01_Historian_Hysteria/01.py
+++ aoc_2024_python/01_Historian_Hysteria/01.py
@@ -1,30 +1,35 @@
+from collections import Counter
+
+
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().splitlines()
-        a,b = [], []
+        a, b = [], []
         for d in data:
             n = d.split()
             a.append(int(n[0]))
             b.append(int(n[1]))
-    return a,b
+    return a, b
 
 
 def get_answer_1(a, b):
-    a.sort()
-    b.sort()
-    print(a,b)
+    _a = sorted(a)
+    _b = sorted(b)
     res = 0
-    for i,j in zip(a,b):
-        print(abs(i-j))
-        res += abs(i-j)
+    for i, j in zip(_a, _b):
+        res += abs(i - j)
     return res
 
 
+def get_answer_2(a, b):
+    counter = Counter(b)
+    return sum(x * counter[x] for x in a)
+
 
 def main():
-    a,b = get_data("input")
-    print(a,b)
-    print(get_answer_1(a,b))
+    a, b = get_data("input")
+    print(get_answer_1(a, b))
+    print(get_answer_2(a, b))
 
 
 if __name__ == "__main__":
